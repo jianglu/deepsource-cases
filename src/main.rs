65be055fb74679fd34a37ccd47333c4d5bc4cadf
Jiang Lu <jianglu@ensurebit.com>
2022-12-16 14:59:02 +0800

    U

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
+    let _ = (0..5).fold(false, |acc, x| acc || x > 2);
+
+    // is better written as
+    // let _ = (0..5).any(|x| x > 2);
+
+    std::env::var("RUST_BACKTRACE").unwrap();
+    // elsewhere ...
+    std::env::remove_var("RUST_BACKTRCAE"); // oops, misspelled
+
     unsafe {
         let p = libc::malloc(100) as *mut u32;
         libc::free(p as _);
